79fec069106514cfd7d59815d902e85406fea364
alvations <alvations@gmail.com>
2015-07-10 08:41:02 +0200

    updated term.py

--- terminator/extract.py
+++ terminator/extract.py
@@ -1,15 +1,38 @@
 
-import io, sys
+import io, sys, time
 import operator
-from itertools import chain
 from collections import Counter
+from itertools import chain
 
-import kenlm
-
-from association_measures import pointwise_mutual_information as pmi
 from association_measures import logprob, lmpmi, lmpmi_freq
-from filter import noun_phrase_filter, simple_filter, low_score_filter
+from filter import noun_phrase_filter, simple_filter
+
+def fame(textfile, model, measure=lmpmi):
+    extracted_terms_counts = Counter()
+    extracted_terms_measure = {}
+    start = 0
+    with io.open(textfile, 'r', encoding='utf8') as fin:
+        for line_count, line in enumerate(fin):
+            if line_count == 0 or line_count % 50 == 0:
+                start = time.time()
+                print line_count,
+            if line_count != 0 and line_count % 500 == 0:
+                print 'took', time.time() - start, 'secs'
+                start = 0
+                print
+            noun_phrases = noun_phrase_filter(line)
+            for ng in simple_filter(noun_phrases):
+                if ng not in extracted_terms_measure:
+                    score = measure(ng, model)
+                    extracted_terms_measure[ng] =score
+                else:
+                     score = extracted_terms_measure[ng]
+                if score !=0:
+                    extracted_terms_counts[ng]+=1
+    return extracted_terms_counts, extracted_terms_measure            
     
+    
+
 def lame(text, model, measure=lmpmi):
     ranked_term_candidates = {}
     noun_phrases = noun_phrase_filter(text)
@@ -19,37 +42,18 @@ def lame(text, model, measure=lmpmi):
             ranked_term_candidates[ng] =  score
     return sorted(ranked_term_candidates.items(), key=operator.itemgetter(1), 
                   reverse=True)
-    
-model_file = '/home/alvas/test/food.arpa'
-textfile = '/home/alvas/test/food.txt'
-model = kenlm.LanguageModel(model_file)
 
-terms2lmpmi = {}
-extracted_terms = Counter()
-with io.open(textfile, 'r', encoding='utf8') as fin:
-    for count, line in enumerate(fin):
-        if line.strip():
-            print count, line.strip()
-            x = lame(line.strip(), model, measure=lmpmi)
-            y = lame(line.strip(), model, measure=lmpmi_freq)
-            fx = low_score_filter(x, 4.0)
-            fy = low_score_filter(y, 0.0001)
-            extracted_terms.update(x)
-            print x, '\n', y, '\n', fx, '\n', fy, '\n'
-
-if __name__ == '__main__':
-    RED, NATIVE = '\033[01;31m', '\033[m'
-    def err_msg(txt):
-        return RED + txt + NATIVE
-
-    if len(sys.argv) is not 3:
-        usage_msg = err_msg('Usage: python3 %s language_model textfile \n'
-                            % sys.argv[0])
-
-        example_msg = err_msg('Example: python3 %s food.arpa food.txt' 
-                              % sys.argv[0])
-        sys.stderr.write(usage_msg)
-        sys.stderr.write(example_msg)
-        sys.exit(1)
-
-    main(*sys.argv[1:])
+def extract_line_by_line(textfile, model, method=lame):
+    with io.open(textfile, 'r', encoding='utf8') as fin:
+        for count, line in enumerate(fin):
+            if line.strip():
+                extracted_terms = method(line.strip(), model, measure=lmpmi)
+                #y = lame(line.strip(), model, measure=lmpmi_freq)
+                #fx = low_score_filter(x, 4.0)
+                #fy = low_score_filter(y, 0.0001)
+                yield line.strip(), extracted_terms
+                
+def extract_for_whole_file(textfile, model, method=fame):
+    extracted_terms_counts, extracted_terms_measure  = method(textfile, model, measure=lmpmi)
+    for ng in sorted(extracted_terms_counts):
+        yield ng, extracted_terms_measure[ng], extracted_terms_counts[ng] 
---
+++ terminator/extract_terms.py
@@ -0,0 +1,38 @@
+
+import io
+from collections import Counter
+
+import kenlm
+
+from extract import extract_line_by_line, extract_for_whole_file
+
+from association_measures import lmpmi, lmpmi_freq
+from filter import low_score_filter
+
+    
+model_file = '/home/alvas/test/food.arpa'
+textfile = '/home/alvas/test/food.txt'
+model = kenlm.LanguageModel(model_file)
+
+#for line, terms in extract_line_by_line(textfile, model):
+#    print '\t'.join([line, str(terms), str(low_score_filter(terms, 4.0))])
+    
+for term, score, count in extract_for_whole_file(textfile, model):
+    print '\t'.join([term, score, count])
+
+if __name__ == '__main__':
+    RED, NATIVE = '\033[01;31m', '\033[m'
+    def err_msg(txt):
+        return RED + txt + NATIVE
+
+    if len(sys.argv) is not 3:
+        usage_msg = err_msg('Usage: python3 %s language_model textfile \n'
+                            % sys.argv[0])
+
+        example_msg = err_msg('Example: python3 %s food.arpa food.txt' 
+                              % sys.argv[0])
+        sys.stderr.write(usage_msg)
+        sys.stderr.write(example_msg)
+        sys.exit(1)
+
+    main(*sys.argv[1:])
